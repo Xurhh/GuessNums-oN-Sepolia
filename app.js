0c2c23a87cf9c2ee88ff4740ccb0c41055a17f5c
xrh <2985496117@qq.com>
2026-01-13 23:48:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,224 @@
+const contractAddress = "0x432602BA6cAAB1217436C214B78D375d62F21E0D";
+const contractABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "guess",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "correct",
+        "type": "bool"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "prize",
+        "type": "uint256"
+      }
+    ],
+    "name": "GuessResult",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "getPrizePool",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_guess",
+        "type": "uint256"
+      }
+    ],
+    "name": "guess",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "guessFee",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "playerBalances",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "prizePool",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "randomNumber",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "max",
+        "type": "uint256"
+      }
+    ],
+    "name": "setDifficulty",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+
+let provider, signer, contract;
+let guessedNumbers = new Set(); // 存储猜过的数字
+
+async function init() {
+    if (typeof window.ethereum !== 'undefined') {
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        signer = provider.getSigner();
+        contract = new ethers.Contract(contractAddress, contractABI, signer);
+        updateBalances();
+        createGrid();
+    } else {
+        alert("请安装MetaMask！");
+    }
+}
+
+async function updateBalances() {
+    const prizePool = await contract.getPrizePool();
+    document.getElementById("prizePool").textContent = ethers.utils.formatEther(prizePool);
+    const userBalance = await signer.getBalance();
+    document.getElementById("userBalance").textContent = ethers.utils.formatEther(userBalance);
+}
+
+function createGrid() {
+    const grid = document.getElementById("numberGrid");
+    grid.innerHTML = "";
+    for (let i = 1; i <= 100; i++) {
+        const div = document.createElement("div");
+        div.textContent = i;
+        if (guessedNumbers.has(i)) {
+            div.classList.add("guessed");
+        }
+        grid.appendChild(div);
+    }
+}
+
+document.getElementById("guessBtn").addEventListener("click", async () => {
+    const guess = parseInt(document.getElementById("guessInput").value);
+    if (!guess || guess < 1 || guess > 100) {
+        alert("请输入1-100的数字！");
+        return;
+    }
+    if (guessedNumbers.has(guess)) {
+        alert("你已经猜过这个数字了！");
+        return;
+    }
+    try {
+        document.getElementById("result").textContent = "正在付款...";
+        const tx = await contract.guess(guess, { value: ethers.utils.parseEther("0.001") });
+        document.getElementById("result").textContent = "付款成功，等待确认...";
+        await tx.wait();
+        guessedNumbers.add(guess);
+        createGrid();
+        updateBalances();
+        // 监听事件
+        contract.on("GuessResult", (player, guessNum, correct, prize) => {
+            if (player.toLowerCase() === signer.getAddress().toLowerCase()) {
+                document.getElementById("result").textContent = correct ? `猜对了！赢得 ${ethers.utils.formatEther(prize)} ETH` : "猜错了，再试试！";
+            }
+        });
+    } catch (error) {
+        document.getElementById("result").textContent = "错误：" + error.message;
+    }
+});
+
+init();
